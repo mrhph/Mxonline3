ba77a82a3d5fe6b9092be61e821b6c68f5d75d91
hph <mrhph@qq.com>
2019-04-24 17:14:34 +0800

    '4.24'

--- Mxonline3/urls.py
+++ Mxonline3/urls.py
@@ -38,11 +38,11 @@ urlpatterns = [
     # 注册
     path('register/', RegisterView.as_view(), name='register'),
     # 激活用户
-    re_path('active/(?P<active_code>.*)/', ActiveUserView.as_view(), name='user_active'),
+    re_path(r'active/(?P<active_code>.*)/', ActiveUserView.as_view(), name='user_active'),
     # 忘记密码
     path('forget/', ForgetPwdView.as_view(), name='forget_pwd'),
     # 重置密码url：用来接收来自邮箱的重置链接
-    re_path('reset/(?P<active_code>.*)/', ResetView.as_view(), name='reset_pwd'),
+    re_path(r'reset/(?P<active_code>.*)/', ResetView.as_view(), name='reset_pwd'),
     # 修改密码, 只接收post请求
     path('modify_pwd/', ModifyPwdView.as_view(), name='modify_pwd'),
 
